798b9789468c6c759c84b6a36416f8f9d5a68ff0
Aoi Nishi <n1sh1_works@yahoo.co.jp>
2021-11-05 23:05:01 +0900

    headerの色切替を下層に対応

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,12 +1,33 @@
 // ヘッダー色変更
+
+// トップページのmv高さ
+let topmv = $('.header').parents('.top');
 let windowHeight = $(window).height();
-jQuery(window).on('scroll', function(){
-  if(windowHeight < jQuery(this).scrollTop()) {
-    jQuery('.header_image').addClass('active');
-  } else {
-    jQuery('.header_image').removeClass('active');
-  }
-});
+
+// 下層ページのmv高さ
+let articlemv = $('.header').parents('.article');
+let articlemvHeight = articlemv.find('.mvContainer').height();
+
+if($('body').hasClass('top')){
+  // .headerが.topの中にあるとき＝トップで実行
+  jQuery(window).on('scroll', function(){
+    if(windowHeight < jQuery(this).scrollTop()) {
+      jQuery('.header_image').addClass('active');
+    } else {
+      jQuery('.header_image').removeClass('active');
+    }
+  });
+}
+if($('body').hasClass('article')){
+  // .headerが.articleの中に合ったら実行
+  jQuery(window).on('scroll', function(){
+    if(articlemvHeight < jQuery(this).scrollTop()) {
+      jQuery('.header_image').addClass('active');
+    } else {
+      jQuery('.header_image').removeClass('active');
+    }
+  });
+}
 
 // SPハンバーガーメニュー
 let menuBar = document.getElementById('menuBar');
